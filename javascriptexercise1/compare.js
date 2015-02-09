ced51824f7149b3b0ef4a7d0988144b0d3f588ab
isiaq <isiaq@dealdey.com>
2015-02-09 09:28:46 +0100

    javasciptexercise1

--- javascriptexercise1/compare.js
+++ javascriptexercise1/compare.js
@@ -1,19 +1,21 @@
-function user(name, age)
+function User(name, age)
 {
   this.name = name;
   this.age = age;
 }
-var user1 = new user("gbenga", 27)
-var user2 = new user("segun", 33)
+var user1 = new User('gbenga', 27)
+var user2 = new User('segun', 33)
+var user3 = new User ('osifo',23)
 
-user.prototype.compare = function(username){
+User.prototype.compare = function(username){
 
   if (user1.age > user2.age)
-    alert(user1.name +"is older than"+user2.name)
+    alert(user1.name +'is older than' +user2.name)
   else if(user1.age == user2.age)
-    alert(user1.name +" is the same age as"+ user2.name)
+    alert(user1.name +' is the same age as'+ user2.name)
   else
-    alert(user2.name+" is older than "+ user1.name) 
+    alert(user2.name+ 'is older than ' + user1.name) 
 
 }
 user1.compare(user2)
+user2.compare(user3)

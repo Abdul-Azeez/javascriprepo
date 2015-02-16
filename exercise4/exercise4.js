5f4642004dacca30513e06c2cf2bb1f407bc8cd7
isiaq <isiaq@dealdey.com>
2015-02-16 10:05:59 +0100

    Exercise 4

--- exercise4/exercise4.js
+++ exercise4/exercise4.js
@@ -1,33 +1,26 @@
-function Urluser()
-{
-  this.person = prompt("Please enter your URL", "http://")
+function Urluser() {
+  this.person = prompt("Please enter your URL", "")
 }
   
 Urluser.prototype.validate = function(e) {
-                    switch(e && e.trim()) {
-                        case "":
-                        case 0:
-                        case "0":
-                        case null:
-                        case false:
-                        case typeof this == "undefined":
-                            return true;
-                                default : return false;
-                    }
-                }
+  switch(e && e.trim()) {
+    case "":
+    case null:
+    return true;
+    default : return false;
+  }
+}
 
 var windowObjectReference;
 var strWindowFeatures = "menubar=no,location=no,resizable=no,scrollbars=no,status=no,toolbar=no, width=400px height=450px";
 
 Urluser.prototype.openRequestedPopup=  function() {
-  if (this.validate(this.person)== true) {
-    while (true) {
+  if (this.validate(this.person)) {
     alert("Please you inputed an empty string")
-    return;
+    return;  
   }
-  }
-  windowObjectReference = window.open(this.person, "Openurl", strWindowFeatures);
-
+  var url = "http://"+ this.person
+  windowObjectReference = window.open(url, "Openurl", strWindowFeatures);
 }
 
 object1= new Urluser()

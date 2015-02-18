c9ecc21a346e8399d063e531adf4128d43cda652
isiaq <isiaq@dealdey.com>
2015-02-18 13:45:20 +0100

    Changes made

--- javascriptexercise1/compare.js
+++ javascriptexercise1/compare.js
@@ -1,23 +1,27 @@
 function User(name, age) {
   this.name = name;
-  this.age = age;
+  this.age = Number(age);
 }
 
 User.prototype.compare = function(user) {
-
-  if (this.age > user.age) {
-    alert(this.name +"is older than"+user.name)
+ if (this.age > user.age) {
+    alert(this.name +" is older than "+user.name)
   }
-  else if (this.age == user.age) {
-    alert(this.name +" is the same age as"+ user.name)
+ else if (this.age == user.age) {
+    alert(this.name +" is the same age as "+ user.name)
   }
-  else {
-    alert(this.name+" is older than "+ user.name) 
+ else {
+    alert(user.name+" is older than "+ this.name) 
   }
 
 }
 
-var user1 = new user("gbenga", 27)
-var user2 = new user("segun", 33)
+var user1 = new User("gbenga", 27)
+var user2 = new User("segun", 33)
+var user3= new User("osifo", 35)
+var user4 = new User("abu", 27)
 
 user1.compare(user2)
+user4.compare(user2)
+user3.compare(user4)
+user2.compare(user1)

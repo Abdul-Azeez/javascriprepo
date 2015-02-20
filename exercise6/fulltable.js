66f1fa635152c20faca23e165265f73e2ce2852e
isiaq <isiaq@dealdey.com>
2015-02-20 17:37:21 +0100

    Exercise6

--- exercise6/fulltable.js
+++ exercise6/fulltable.js
@@ -1,17 +1,24 @@
 function Rows() {
 }
 
-Rows.prototype.SelectMoveRows= function(SS1,SS2)
+Rows.prototype.SelectMoveRows = function(SS1,SS2)
 {
-    // Move rows from SS1 to SS2 from bottom to top
-  for (i=SS1.options.length - 1; i>=0; i--)
+  // Move rows from SS1 to SS2 from bottom to top
+  for (i=SS1.options.length - 1; i >= 0; i--)
   {
     if (SS1.options[i].selected == true)
     {
         SS2.appendChild(SS1.options[i])
     }
   }
-   
 }
-var Row1 = new Rows()
-var Row2 = new Rows()
+
+var Row1 = new Rows();
+submitA = document.getElementById("button1");
+submitB = document.getElementById("button2");
+submitA.addEventListener("click", function() { 
+  Row1.SelectMoveRows(document.Countries.SetofcountriesA, document.Countries.SetofcountriesB)
+});
+submitB.addEventListener("click", function() {
+ Row1.SelectMoveRows(document.Countries.SetofcountriesB, document.Countries.SetofcountriesA)
+});

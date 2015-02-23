1eaceb509d60ca924c28fe34981962cee2ea19d0
isiaq <isiaq@dealdey.com>
2015-02-23 10:34:16 +0100

    exercise5 changes

--- exercise5/exercise5.js
+++ exercise5/exercise5.js
@@ -9,21 +9,19 @@ User.prototype.validate = function(e) {
     case null:
     return true;
     default : return false;
-    }  
+  }  
 }
-
 
 User.prototype.print = function () {
 
   if (this.validate(this.firstname)) {
-    (this.firstname= prompt("Please enter your first name", ""));
-    
+    (this.firstname= prompt("Please enter your first name", ""));   
      preventDefault();
   }
   if (this.validate(this.lastname)) {
       this.lastname= prompt("Please enter your lastname name", "");
       preventDefault();
-    }
+  }
   document.getElementById("demo").innerHTML =
   "Hello " + this.firstname +" "+ this.lastname;
 }

aa8e71ff52d7b3a2cd51a2ecc7a4e6a32c92f8a5
isiaq <isiaq@dealdey.com>
2015-02-24 12:08:17 +0100

    domain changes

--- Domain/Domain.js
+++ Domain/Domain.js
@@ -6,11 +6,18 @@
 
     var string = document.getElementById('checkurl').value;
 
-    var reg = /^(?:https?\:\/\/)?((\w+\.)*)(\w+\.\w+)/i;
+    // var reg = /^(?:https?:\/\/www.)?(\w+\.)*(\w+\.\w+)/i;
+    var reg = /^(?:https?:\/\/www.|www.)?(\w+\.)*(\w+\.\w+)/i;
     if(reg.test(string))
     {
-       alert((RegExp.$1.length == 0) ? 'Domain: '+RegExp.$3 : 'Subdomain: '+RegExp.$1+' Domain: '+RegExp.$3);
-      // alert(RegExp.$1+'-'+RegExp.$2+'-'+RegExp.$3);
+      //  // alert((RegExp.$1.length == 0) ? 'Domain: '+RegExp.$3 : 'Subdomain: '+RegExp.$1+' Domain: '+RegExp.$2);
+      // alert(RegExp.$1+'-first'+RegExp.$2+'-second'+RegExp.$3);
+      if(RegExp.$1.length==0){
+        alert("Domain:"+ RegExp.$2)
+      } else {
+        alert("Subdomain:" + RegExp.$1 +"Domain" +RegExp.$2)
+      }
+
     }
     else
     {

64bddcb15590bc5caa79f739308b398a01bab957
isiaq <isiaq@dealdey.com>
2015-02-26 11:55:32 +0100

    javascript exercise

--- javascriptexercise3/checkbox2.js
+++ javascriptexercise3/checkbox2.js
@@ -1,37 +1,55 @@
-// <script>
-function checkBox() {
-
+function CheckBox() {
 
 }
+CheckBox.prototype.chkControl = function() {
+  var total = 0;
+  var days = [];
+  var message = ""
+  var none = document.getElementById('none');
+  none.checked = false;
+  for (var i = 0; i < document.myForm.myCheckbox.length; i++) {
+    total = total + 1;
+    if (document.myForm.myCheckbox[i].checked) {
+      day = document.myForm.myCheckbox[i].id
+      days.push(day);
+      if(total > 3) {
+        alert("you can choose more than 3 days. You have already choosen" + "  " + days[0]+" "+ days[1]+" "+ days[2])
+        document.getElementById(days[3]).checked = false;
+      } 
+    } else {
+       total--;
+  }
+     
+      // days.pop(day);
+  } 
+}
 
-checkBox.prototype.SetAllCheckBoxes= function(FormName, FieldName, CheckValue)
+CheckBox.prototype.noCheckBox= function(FormName, FieldName)
 {
-  if(!document.forms[FormName])
+  var nonevalue = document.getElementById("none")
+  nonevalue.checked = false;
+  if(!document.forms[FormName]) {
     return;
+  }
   var objCheckBoxes = document.forms[FormName].elements[FieldName];
-  if(!objCheckBoxes)
+  if(!objCheckBoxes) {
     return;
-  var countCheckBoxes = objCheckBoxes.length;
-  if(!countCheckBoxes)
-    objCheckBoxes.checked = CheckValue;
-  else
-    // set the check value for all check boxes
-    for(var i = 0; i < countCheckBoxes; i++)
-      objCheckBoxes[i].checked = CheckValue;
+  }
+ var countCheckBoxes = objCheckBoxes.length;
+}
+// // Unchecked all the documents when none is clicked//
+CheckBox.prototype.uncheckAll = function () {
+  if(document.getElementById('none').checked == true) {
+    countCheckBoxes = 0;
+    var inputFields = document.getElementsByTagName("input");
+    for (var i = 0; i < 7; i++) {
+      inputFields[i].checked = false;
+    }
+  }
 }
 
-
-function chkcontrol(j) {
-var total=0;
-for(var i=0; i < document.myForm.myCheckbox.length; i++){
-if(document.myForm.myCheckbox[i].checked){
-total =total +1;}
-if(total > 3){
-alert("Please Select only three")
-document.myForm.myCheckbox[j].checked = false;
-return false;
+CheckBox.prototype.checkAll = function() {
+  this.noCheckBox('myForm', 'myCheckbox');
+  this.chkControl();
 }
-}
-} 
-
-var checkbox1 = new checkBox();
+var checkBox1 = new CheckBox();

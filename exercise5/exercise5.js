737b308fced45d1b0d578c221f0c197948fc1878
isiaq <isiaq@dealdey.com>
2015-02-27 10:50:17 +0100

    exercise5

--- exercise5/exercise5.js
+++ exercise5/exercise5.js
@@ -1,31 +1,34 @@
-function User(firstname, lastname) {
-  this.firstname = prompt("Please enter your first name", "")
-  this.lastname = prompt("Please enter your lastname name", "")
+function User() {
+  this.firstname = ""
+  this.lastname = ""
 }
 
 User.prototype.validate = function(e) {
   switch(e && e.trim()) {
-    case "":
+    case "": 
     case null:
     return true;
     default : return false;
   }  
 }
 
-User.prototype.print = function () {
 
-  if (this.validate(this.firstname)) {
-    (this.firstname= prompt("Please enter your first name", ""));   
-     preventDefault();
+User.prototype.print = function () {
+  for (var i = 0; i<names.length; i++) {
+    var input = prompt('Enter your ' + names[i], " ");
+    if (!this.validate(input)) {
+      this[names[i]] = input;
+      if (this.firstname && this.lastname) {
+        document.getElementById("demo").innerHTML =
+        "Hello " + this.firstname +" "+ this.lastname;
+      }
+    } else {
+      alert(names[i]+ "can't be empty")
+    }
   }
-  if (this.validate(this.lastname)) {
-      this.lastname= prompt("Please enter your lastname name", "");
-      preventDefault();
-  }
-  document.getElementById("demo").innerHTML =
-  "Hello " + this.firstname +" "+ this.lastname;
 }
 
-var user1 = new User()
+var user1 = new User();
+var names = ["firstname", "lastname"];
 
-user1.print()
+user1.print();

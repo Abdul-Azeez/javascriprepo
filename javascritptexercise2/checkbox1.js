6f3b905551d1059b2b85c85f6065761b6b719171
isiaq <isiaq@dealdey.com>
2015-02-27 11:21:43 +0100

    exercise2

--- javascritptexercise2/checkbox1.js
+++ javascritptexercise2/checkbox1.js
@@ -5,18 +5,24 @@ function ToggleCheck() {
 ToggleCheck.prototype.checkBoxHandler = function(FormName, FieldName, CheckValue) {
   var checkBoxElements = document.forms[FormName].elements[FieldName];
   var noOfElements = checkBoxElements.length;
-  if (!document.forms[FormName]) {
-    return;
-  }  
-  if (!checkBoxElements) {
-    return;
-  } else {
+  if (this.checkFormName && this.checkFormElementName) {
     // set the check value for all check boxes
     for(var i = 0; i < noOfElements; i++) {
       checkBoxElements[i].checked = CheckValue;
     }
   }
 }
+ToggleCheck.prototype.checkFormName = function(FormName) {
+    if (!document.forms[FormName]) {
+    return;
+  } 
+} 
+ToggleCheck.prototype.checkFormElementName = function(FieldName) {
+    if (!checkBoxElements) {
+    return;
+  }
+}
+
 
 var setOfToggleChecks = new ToggleCheck();
 var button = document.getElementById("button");

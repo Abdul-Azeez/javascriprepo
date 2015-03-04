6f70b32d7045410da8f15926b10fbbc6ab4da52c
isiaq <isiaq@dealdey.com>
2015-03-04 14:56:40 +0100

    Changes on names and childnodes

--- ParentChild/parentchild.js
+++ ParentChild/parentchild.js
@@ -5,21 +5,20 @@
   { 
     var boxs = box + "Tick";
     var elements = document.getElementById(boxs);
-    console.log(elements);
-    var tChid = document.getElementById(box+'All');
-    if(tChid.checked)
+    var selectedGroup = document.getElementById(box+'All');
+    if(selectedGroup.checked)
     {
       var number = Number (10)
       elements.style.display = 'block';
-      tChid.scrollIntoView(true);
+      selectedGroup.scrollIntoView(true);
     }
     else
     {
       elements.style.display = 'none';
     }
-    var nodes = elements.childNodes;
+    var nodes = elements.querySelectorAll('input[type=checkbox]');
     for(i=0; i<nodes.length; i++) {
-      nodes[i].checked = tChid.checked;
+      nodes[i].checked = selectedGroup.checked;
     }  
   }
 

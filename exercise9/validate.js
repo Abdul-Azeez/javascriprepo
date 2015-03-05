b5e655a7aaf7070ff55e69d1fb9889fc1a3f25b6
isiaq <isiaq@dealdey.com>
2015-03-05 18:07:14 +0100

    validation

--- exercise9/validate.js
+++ exercise9/validate.js
@@ -1,19 +1,17 @@
-function Form() {
-  
-}
-
-Form.prototype.validateLenght = function (lenght1) {
-	this.lenght1 = lenght1;
-	if ((lenght1.length) < 50) {
-		alert("string lenght must not be less than 50.")
-	}
+function Form() { 
+  var formbox = document.getElementById("formbox");
+  formbox.addEventListener("submit", function() { 
+    form1.validateAll();
+    form1.validateEmail();
+    form1.validateUrl();
+  });
 }
 
 Form.prototype.validateEmail = function(email) {
   var email = document.getElementById("email")
   var regex = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/
   var match = regex.test(email.value);
-  if (match == false) {
+  if (match == false && email.value != "" ) {
       alert("Please use the proper email format")
   }
 }
@@ -22,53 +20,37 @@ Form.prototype.validateUrl = function(homepage) {
   var url = document.getElementById("homepage")
   var regex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
   var match = regex.test(url.value);
-  if (match==false) {
+  if (match==false && url.value != "" ) {
     alert("Please use proper Url format")
   }
 }
 
 Form.prototype.validateAll = function(elements) {
-  var elements = document.forms.elements;
-  for (var i = 0; i < elements.length - 1; i++) { 
-    if (forms.elements[i].value == "" || forms.elements[i].value== null) {
-      if(forms.elements[i].id !== "timezone" && forms.elements[i].id !== "notify") {
-        alert(forms.elements[i].id+ "  can't be empty");
-        break;
-      }  
-      if (forms.elements[i].id == "notify") {
-        this.validateCheckbox();
-        break;
-      }
-      if (forms.elements[i].id == "aboutme") {
-        this.validateAboutme();
-        break;
-      }
-      if (forms.elements[i].id == "email") {
-        this.validateEmail("email")
-        break;
-      }
-      if (forms.elements[i].id == "url") {
-        this.validateUrl("url")
-        break;
-      }
-    }
-  }
-}
+ var email = document.getElementById("email");
+var url = document.getElementById("homepage");
+  if (document.forms.loginid.value == "") {
+    alert("login Id can't be empty")
+    event.preventDefault();
+  } else if(document.forms.email.value == "") {
+    alert("Email can't be empty")
+    event.preventDefault();
+  } else if(document.forms.name.value == "") {
+    alert("Name can't be empty")
+    event.preventDefault();
+  } else if (document.forms.homepage.value == ""){
+    alert("Homepage can't be empty")
+    event.preventDefault();
+  } else if (document.forms.aboutme.value == "") {
+    alert("Please check your aboutme filed")
+    event.preventDefault();
+  } else if ((document.forms.aboutme.value).length < 50 ) {
+    alert("Please the string lenght of the aboutme field must be longer than 50 characters..")
+    event.preventDefault();
+  } else if (document.getElementById("notify").checked == false ) {
+    alert("Please check the notify field")
+    event.preventDefault();
+  } 
 
-Form.prototype.validateCheckbox = function() {
-  var notify = document.getElementById("notify")
-  if (notify.checked==false ) {
-    alert("Please click the checkbox")
-  }
+  // return true;
 }
-
-Form.prototype.validateAboutme = function() {
-  var aboutme = document.getElementById("aboutme");
-  this.validateLenght("aboutme");
-}
-
 var form1 = new Form()
-var form1 = new Form()
-var formbox = document.getElementById("formbox");
-formbox.addEventListener("submit", function() { form1.validateAll()}, true );
-// form1.print()

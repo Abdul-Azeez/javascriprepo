c9ce736b373c1975ee75b746c1f4743d7c5ec290
isiaq <isiaq@dealdey.com>
2015-03-05 15:58:34 +0100

    Javascript

--- javascriptexercise3/checkbox2.js
+++ javascriptexercise3/checkbox2.js
@@ -1,40 +1,62 @@
-function CheckBox() {
-
+function CheckBox(checkBoxes, noneCheckbox ) {
+  this.maxDays = 3;
+  this.selectedDays = [];
+  this.checkBoxes = checkBoxes;
+  this.noneCheckbox = noneCheckbox;
 }
-CheckBox.prototype.chkControl = function() {
-  var total = 0;
-  var days = [];
-  var message = ""
-  var none = document.getElementById('none');
-  none.checked = false;
-  for (var i = 0; i < document.myForm.myCheckbox.length; i++) {
-    total = total + 1;
-    if (document.myForm.myCheckbox[i].checked) {
-      day = document.myForm.myCheckbox[i].id
-      days.push(day);
-      if(total > 3) {
-        alert("you can choose more than 3 days. You have already choosen" + "  " + days[0]+" "+ days[1]+" "+ days[2])
-        document.getElementById(days[3]).checked = false;
-      } 
-    } else {
-      total--;
-    } 
-     
-  } 
+
+CheckBox.prototype.initialze = function(){
+  this.noneCheckbox.checked = true;
+}
+
+CheckBox.prototype.addEventHandlers = function() {
+  var that = this;
+
+  //handling clicks
+  this.noneCheckbox.addEventListener('click', function(){
+    if(this.checked) {
+      that.uncheckAll();
+    }
+  });
+
+  //handling clicks from all the other checkbox items
+  for(var i = 0; i < this.checkBoxes.length; i++) {
+    this.checkBoxes[i].addEventListener('click', function(){
+      if(this.checked){
+        if(that.selectedDays.length < that.maxDays) {
+          that.selectedDays.push(this.id);
+          that.noneCheckbox.checked = false;//unchecks the none checkbox due to a click on another checkbox
+          console.log(that.selectedDays);
+        }else {
+          alert("you can choose more than 3 days. You have already choosen" + "  " + that.selectedDays[0]+" "+ that.selectedDays[1]+
+            " "+ that.selectedDays[2]);
+          this.checked = false;
+        }
+      } else {
+        that.selectedDays.splice(that.selectedDays.indexOf(this.id),1);//removes target item from the list.
+      }
+    });
+  }
 }
 
 // // Unchecked all the documents when none is clicked//
 CheckBox.prototype.uncheckAll = function () {
-  if(document.getElementById('none').checked == true) {
-    countCheckBoxes = 0;
-    var inputFields = document.getElementsByTagName("input");
-    for (var i = 0; i < 7; i++) {
-      inputFields[i].checked = false;
-    }
+  for (var i = 0; i < this.checkBoxes.length; i++) {
+    this.checkBoxes[i].checked = false;
   }
+  this.selectedDays= [];
 }
 
 CheckBox.prototype.checkAll = function() {
   this.chkControl();
 }
-var checkBox1 = new CheckBox();
+
+
+//preparing the DOM to handle class functionalites
+document.addEventListener('DOMContentLoaded', function(){
+  var checkBoxes = document.getElementsByName('myCheckbox');
+  var noneCheckbox = document.getElementById('none');
+  var checkBox1 = new CheckBox(checkBoxes, noneCheckbox);
+  checkBox1.addEventHandlers();
+  checkBox1.initialze();
+});

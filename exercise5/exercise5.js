d2f1706b2948da6bc6154bb986e46fa5879fdae3
isiaq <isiaq@dealdey.com>
2015-03-06 11:02:31 +0100

    Changes made

--- exercise5/exercise5.js
+++ exercise5/exercise5.js
@@ -2,17 +2,25 @@ function User(names) {
   this.names = ["firstname", "lastname"];
 }
 
+User.prototype.validate = function(input) {
+  if (input !=null) {
+    if (input.trim() =="") {
+      return true;
+    }
+  } 
+}
+
 User.prototype.print = function () {
   for (var i = 0; i<this.names.length; i++) {
     do {
       var input = prompt('Enter your ' + this.names[i]);
     }
-    while ((input == "" ));
+    while (this.validate(input));
     if (input == null) {
-      alert(this.names[i]+ "can't be empty")
+      alert(this.names[i]+ " can't be empty")
       break;
     }
-    if ((input != "" || input!= null)) {
+    if (!this.validate(input)) {
       this[this.names[i]] = input;
       if (this.firstname && this.lastname) {
         document.getElementById("demo").innerHTML =
@@ -23,6 +31,4 @@ User.prototype.print = function () {
 }
 
 var user1 = new User();
-
-
 user1.print();

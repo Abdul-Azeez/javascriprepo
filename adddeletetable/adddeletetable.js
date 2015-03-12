ea6f3ad9da31c65d62832f4dde9ee7c5a53bec0e
isiaq <isiaq@dealdey.com>
2015-03-12 14:26:39 +0100

    some modifications done

--- adddeletetable/adddeletetable.js
+++ adddeletetable/adddeletetable.js
@@ -18,7 +18,6 @@ Table.prototype.createText = function() {
 Table.prototype.addCell = function() {
   tableObj = this.addRow();
   tableObj.rowObj.id = "row"+tableObj.lastRow;
-  console.log(tableObj.rowObj.id);
   this.createCells( 0,"name");
   this.createCells(1, "email");
   var cell3 = tableObj.rowObj.insertCell(2);
